4334219e257e876720f189c219bc4b02bf8fa876
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-02-27 07:40:01 -0300

    implementando spring security

---
+++ src/main/java/med/voll/api/domain/entity/user/User.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.entity.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String password;
+}
